5edf5410a9f077181fab26c52f2445d7dd05d8cb
Danial Heidari <148187114+sphinxorigin@users.noreply.github.com>
2026-08-22 01:00:48 +0200

    Update entrypoint.sh to call renamed startup script

--- htw-network-lab/homerouter/entrypoint.sh
+++ htw-network-lab/homerouter/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /rename_by_subnet.sh "10.10.10.:ensI0" "192.168.0.:ensC0" "192.168.1.:ensS0"
-bash /original_homerouter_startup.sh
+bash /homerouter_startup.sh
 echo "[homerouter] Setup abgeschlossen, Container bleibt aktiv."
 tail -f /dev/null
